293b7b9ddae31584c7db4b4a9a8fb7b245e67583
altinthaqi <altin.thaqi.520@gmail.com>
2021-11-12 00:29:23 +0100

    Finished 2nd Slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -12,5 +12,33 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000);
+}
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  if (n > slides2.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides2.length;
+  }
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
 }
